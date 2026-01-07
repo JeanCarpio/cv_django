6ec92ef129c8cee5f5048528f2b53e1904edfebc
JeanCarpio <berthasuarez459@gmail.com>
2026-01-07 00:38:27 -0500

    Fix AppRegistryNotReady - remove ORM from settings

--- config/settings.py
+++ config/settings.py
@@ -140,18 +140,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import os
 
-if os.environ.get("RENDER") == "true":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
+DJANGO_SUPERUSER_USERNAME = os.getenv("DJANGO_SUPERUSER_USERNAME")
+DJANGO_SUPERUSER_EMAIL = os.getenv("DJANGO_SUPERUSER_EMAIL")
+DJANGO_SUPERUSER_PASSWORD = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-    if username and password and not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
 
